a0692b43c6cfdbff8e63287f404e68dd27b8de43
Saniul <isaniul999@gmail.com>
2026-01-18 16:38:30 +0600

    first commit

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "edoc";
+
+$database = new mysqli($servername, $username, $password, $dbname);
+
+if ($database->connect_error) {
+    die("Connection failed: " . $database->connect_error);
+}
+?>
+
